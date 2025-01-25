e7d74371463d84e2715cd160c6e34f826c9484e0
Munge Kevin <orokomunge@gmail.com>
2025-01-25 09:20:20 +0300

    env files for project

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,17 +1,11 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-import { getDatabase } from 'firebase/database';
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
+import { getFirestore } from "firebase/firestore";
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: "AIzaSyDCB5qWujSymGcrvIfggbdm8Yy_o_PBJM0",
+  apiKey: "AIzaSyDCB5WujSymGcrvIfggbdm8Yy_o_PBJM0",
   authDomain: "civic-voices-todo.firebaseapp.com",
   projectId: "civic-voices-todo",
-  storageBucket: "civic-voices-todo.firebasestorage.app",
+  storageBucket: "civic-voices-todo.appspot.com",
   messagingSenderId: "250080750123",
   appId: "1:250080750123:web:9364b07cd9d247ac27986e",
   measurementId: "G-FLR7J9Y42R"
@@ -19,5 +13,4 @@ const firebaseConfig = {
 
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
-export const db = getDatabase(app);
+export const db = getFirestore(app);
